528060110672d35ffe8e6d05c0d0df904cec110f
Allison <91392629+allisonantunes@users.noreply.github.com>
2023-06-13 09:23:14 -0300

    up

---
+++ 11. Stage 05 - Avançando no JavaScript/jogo de advinhação/script.js
@@ -0,0 +1,12 @@
+/* 
+DOM é a interface api que conecta o html no JS
+
+DOM nao é JS... é a representação do html em formato de um objeto JS
+
+As tags html quando a DOM usa, sao chamadas de: node ou element...
+
+innerText pega o texto do html
+innerHTML pega o html
+
+
+*/
