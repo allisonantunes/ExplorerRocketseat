6645cd24f10364d1913ee2cdd6b111b72513c3a7
allison <allisonabc@hotmail.com>
2023-07-11 17:27:50 -0300

    up

--- 14 Stage 06/02. Avançando nas Promises/GitHube-Favorites/js/Favorites.js
+++ 14 Stage 06/02. Avançando nas Promises/GitHube-Favorites/js/Favorites.js
@@ -1,31 +1,47 @@
-// 1 classe que vai conter a lógica dos dados
-// como os dados serão estruturados
-export class GithubUser{
-    static search(username) {
-        const endpont = `https://api.github.com/users/${username}`
-        return fetch(endpont)
-        .then(data => data.json())
-        .then(({login, name, public_repos, followers}) => ({
-            login,
-            name,
-            public_repos,
-            followers
-        }))
-    }
-}
+import { GithubUser } from "./GithubUser.js"
+
 export class Favorites {
     constructor(root) {
         this.root = document.querySelector(root)
         this.load()
 
-        GithubUser.search('allisonantunes') // vem da pesquisa
+/*         GithubUser.search('allisonantunes') // vem da pesquisa
         .then(user => console.log(user))
-        // pegando a cadeia de promessa do GithubUser... continuando
+        // pegando a cadeia de promessa do GithubUser... continuando */
     }
     load() {
         this.entries = JSON.parse(localStorage.getItem('@github-favorites:')) || []
-        
     }
+
+    save() {
+        localStorage.setItem('@github-favorites:', JSON.stringify(this.entries))
+        // salvar no localstorage
+        // transformar em json o entries
+    }
+
+    async add(username) { // assincrono
+        try {
+            // find = encontre. recebe funcao. devolve um obj
+            const userExists = this.entries.find(entry => entry.login === username)
+
+            if(userExists) {
+                throw new Error('Usuário já cadastrado')
+            }
+
+            const user = await GithubUser.search(username)
+            if(user.login === undefined){
+                throw new Error('Usuario não encontrado')
+            }
+            this.entries = [user, ...this.entries] // add um novo user no novo array e pegando os outros
+            this.update()
+            this.save()
+
+        } catch (error) {
+            alert(error.message)
+        }
+        // await está aguardando uma promessa
+    }
+
     delete(user) {
         // Higher=order functions (map, filter, find, reduse)
         const filteredEntries = this.entries
@@ -33,6 +49,7 @@ export class Favorites {
 
         this.entries = filteredEntries
         this.update()
+        this.save()
     }
 }
 
@@ -43,10 +60,18 @@ export class FavoritesView extends Favorites {
         
         this.tbody = this.root.querySelector('table tbody')
 
-        console.log(this.root);
         this.update()
-        
+        this.onadd()
     }
+
+    onadd() {
+        const addButton = this.root.querySelector('.search button')
+        addButton.onclick = () => {
+            const {value} = this.root.querySelector('.search input')
+            this.add(value)
+        }
+    }
+
     update() {
         this.removeAllTr()
 
@@ -56,8 +81,9 @@ export class FavoritesView extends Favorites {
             row.querySelector('.user img').src = `https://github.com/${user.login}.png`
             row.querySelector('.user img').alt = `Imagem de ${user.name}`
             row.querySelector('.user p').textContent = user.name
+            row.querySelector('.user a').href = `https://github.com/${user.login}`
             row.querySelector('.user span').textContent = user.login
-            row.querySelector('.repositories').textContent = user.public_repor
+            row.querySelector('.repositories').textContent = user.public_repos
             row.querySelector('.followers').textContent = user.followers
 
             row.querySelector('.remove').onclick = () => {
