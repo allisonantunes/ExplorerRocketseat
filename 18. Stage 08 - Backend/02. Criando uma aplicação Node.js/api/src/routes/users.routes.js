d9f0a223c7cbef6435184d936d1ef6daaff4aa98
Allison Antunes <91392629+allisonantunes@users.noreply.github.com>
2023-07-25 17:11:54 -0300

    api

--- 18. Stage 08 - Backend/02. Criando uma aplicação Node.js/api/src/routes/users.routes.js
+++ 18. Stage 08 - Backend/02. Criando uma aplicação Node.js/api/src/routes/users.routes.js
@@ -2,6 +2,16 @@ const { Router } = require('express')
 const UsersController = require('../controllers/UsersController')
 const usersRoutes = Router()
 
+/* function myMiddleware(request, response, next) {
+    console.log('passo no middleware');
+
+    if(!request.body.isAdmin){
+        return response.json({message: "user unauthorized"});
+    }
+
+    next()
+} */
+
 const userController = new UsersController()
 
 usersRoutes.post('/', userController.create)
