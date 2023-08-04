577fe7a4dbb177f19ed9c7c0b596bce7efa1047f
Allison Antunes <91392629+allisonantunes@users.noreply.github.com>
2023-08-04 10:51:35 -0300

    test

--- 18. Stage 08 - Backend/SQL/api/src/controllers/NotesController.js
+++ 18. Stage 08 - Backend/SQL/api/src/controllers/NotesController.js
@@ -11,25 +11,38 @@ class NotesController {
             user_id
         })
 
+        const tagsInsert = tags.map(name => {
+            return {
+                note_id: 1,
+                name: name,
+                user_id: user_id
+            }
+        })
+        await knex('tags').insert(tagsInsert)
+
         const linksInsert = links.map(link => {
             return {
-                note_id,
+                note_id: 1,
                 url: link
             }
         })
         await knex('links').insert(linksInsert)
 
-        const tagsInsert = tags.map(name => {
-            return {
-                note_id,
-                name,
-                user_id
-            }
-        })
-
-        await knex('tags').insert(tagsInsert)
-
         response.json()
+    }
+
+    async show(request, response) {
+        const { id } = request.params
+
+        const note = await knex("notes").where({ id }).first()
+        const tags = await knex("tags").where({ note_id: id}).orderBy("name")
+        const links = await knex("links").where({note_id:id}).orderBy("created_at")
+        
+        return response.json({
+            ...note,
+            tags,
+            links
+        })
     }
 }
 
